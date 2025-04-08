7640d5624f757ae0493b14257559d1fa08330977
aleksandraradeva <aleksandra.b.radeva@gmail.com>
2025-04-08 13:54:30 +0200

    Added register and login auth api

---
+++ client/src/api/auth-api.js
@@ -0,0 +1,25 @@
+import * as fetcher from "./fetcher"; 
+
+const BASE_URL = "http://localhost:3000/api/users";
+
+// Register
+export async function registerUser(email, password) {
+    try {
+        const result = await fetcher.post(`${BASE_URL}/register`, { email, password });
+        const { token, user } = result; 
+        return { token, user };
+    } catch (error) {
+        throw new Error("Registration failed: " + error.message);
+    }
+}
+
+// Login
+export async function loginUser(email, password) {
+    try {
+        const result = await fetcher.post(`${BASE_URL}/login`, { email, password });
+        const { token, user } = result;
+        return { token, user };
+    } catch (error) {
+        throw new Error("Login failed: " + error.message);
+    }
+}
