90a2aac625136185754a009bdb83a855d17e2af2
aleksandraradeva <aleksandra.b.radeva@gmail.com>
2025-04-10 00:28:28 +0200

    Added route guards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import { AuthProvider } from './context/AuthContext';
 
+import PrivateRoute from "./guards/PrivateRoute";
+import GuestRoute from "./guards/GuestRoute";
+
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./components/home/Home";
@@ -11,6 +14,7 @@ import TripCreate from "./components/trip-create/TripCreate";
 import TripDetails from "./components/trip-details/TripDetails";
 import TripEdit from "./components/trip-edit/TripEdit";
 import MyTrips from "./components/mytrips/MyTrips";
+import MyWishlist from "./components/mywishlist/MyWishList";
 import NotFound from "./components/not-found/NotFound";
 
 function App() {
@@ -19,13 +23,45 @@ function App() {
             <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
+                
+                {/* Guests only */}
+                <Route path="/login" element={
+                  <GuestRoute>
+                      <Login />
+                  </GuestRoute>
+                } />
+                <Route path="/register" element={
+                  <GuestRoute>
+                      <Register />
+                  </GuestRoute>
+                  } />
+                
+                {/* Guests & Private */}
                 <Route path="/trips" element={<TripList />} />
-                <Route path="/trips/create" element={<TripCreate />} />
                 <Route path="/trips/:tripId/details" element={<TripDetails />} />
-                <Route path="/trips/:tripId/edit" element={<TripEdit />} />
-                <Route path="/trips/mytrips" element={<MyTrips />} />
+                
+                {/* Private only */}
+                <Route path="/trips/create" element={
+                  <PrivateRoute>
+                      <TripCreate />
+                  </PrivateRoute>
+                  } />
+                <Route path="/trips/:tripId/edit" element={
+                  <PrivateRoute>
+                      <TripEdit />
+                  </PrivateRoute>
+                  } />
+                <Route path="/trips/mytrips" element={
+                  <PrivateRoute>
+                      <MyTrips />
+                  </PrivateRoute>
+                  } />
+                <Route path="trips/mywishlist" element={
+                  <PrivateRoute>
+                      <MyWishlist />
+                  </PrivateRoute>
+                  } />
+
                 <Route path="*" element={<NotFound />} />
             </Routes>
 
---
+++ client/src/guards/GuestRoute.jsx
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
+
+export default function GuestRoute({ children }) {
+    const { isAuthenticated } = useContext(AuthContext);
+
+    if (isAuthenticated) {
+        return <Navigate to="/trips" replace />;
+    }
+
+    return children;
+}
